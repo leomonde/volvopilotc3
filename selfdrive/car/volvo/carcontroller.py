8ba1175754f0bb39c2c9e219ca4d569c6520c016
leomonde <git@gitlab.com>
2024-03-02 01:03:02 -0300

    try SNG

--- selfdrive/car/volvo/carcontroller.py
+++ selfdrive/car/volvo/carcontroller.py
@@ -162,18 +162,18 @@ class CarController():
 
     # SNG
     # send resume at a max freq of 5Hz
-    #if (self.frame - self.last_resume_frame) * DT_CTRL > 0.10:
-    #  if CS.out.cruiseState.standstill and CS.out.vEgo < 0.01 and not self.waiting:
-    #    self.distance = CS.out.cruiseState.accdistance
-    #    self.waiting = True
-    #  if CS.out.cruiseState.standstill and CS.out.vEgo < 0.01 and self.waiting and CS.out.cruiseState.accdistance > self.distance:
-    #    # send 25 messages at a time to increases the likelihood of resume being accepted
-    #    can_sends.extend([volvocan.resumeACC(self.packer, self.CP.carFingerprint, CS, 0)] * 25)
-    #    can_sends.extend([volvocan.checkACC(self.packer, self.CP.carFingerprint, CS, 0)] * 25)
-    #    if (self.frame - self.last_resume_frame) * DT_CTRL >= 0.20:
-    #      self.last_resume_frame = self.frame
-    #  if not CS.out.cruiseState.standstill and self.waiting:
-    #    self.waiting = False
+    if (self.frame - self.last_resume_frame) * DT_CTRL > 0.10:
+      if CS.out.standstill and CS.out.vEgo < 0.01 and not self.waiting:
+        self.distance = CS.out.accdistance
+        self.waiting = True
+      if CS.out.standstill and CS.out.vEgo < 0.01 and self.waiting and CS.out.accdistance > self.distance:
+        # send 25 messages at a time to increases the likelihood of resume being accepted
+        can_sends.extend([volvocan.resumeACC(self.packer, self.CP.carFingerprint, CS, 0)] * 25)
+        can_sends.extend([volvocan.checkACC(self.packer, self.CP.carFingerprint, CS, 0)] * 25)
+        if (self.frame - self.last_resume_frame) * DT_CTRL >= 0.20:
+          self.last_resume_frame = self.frame
+      if not CS.out.standstill and self.waiting:
+        self.waiting = False
 
     # Send diagnostic requests
     if(self.doDTCRequests):
